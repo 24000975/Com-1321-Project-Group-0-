8706da2dba2ca1af67f8670b11c005d4fefa4e80
24000975@mvula.univen.ac.za <163327991+24000975@users.noreply.github.com>
2024-10-07 15:55:56 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+//This class shall contain and store user data & Methods to be shared across all the other child classes
+
+
+    public static Double GrossIncome; // Make GrossIncome a class variable
+    public static Double EstimatedTax; // Make EstimatedTax a class variable
+
+
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+
+        //getting user s data:
+
+        System.out.println("Welcome !! , Please enter the following : ");
+
+        System.out.println("a. Your Gross income(Before deductions) :  ");
+        Double GrossIncome=input.nextDouble();
+
+        System.out.println("b. Estimated Tax deduced(Amount) :  ");
+        Double EstimatedTax=input.nextDouble();
+
+        //We print the income after tax
+
+        Double IncomeAfterTax = calculateIncomeAfterTax(GrossIncome, EstimatedTax);
+        System.out.println("Your Income After Tax is : " + IncomeAfterTax);
+
+
+
+    }
+
+    // Method to calculate IncomeAfterTax
+    public static Double calculateIncomeAfterTax(Double GrossIncome, Double EstimatedTax) {
+        return GrossIncome - EstimatedTax;
+    } 
+}
+    
+
